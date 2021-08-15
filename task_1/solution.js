77fa73a48b0c256d2ca46cc209a488275e6b9920
Evgeniy-Varlamov <varlamov-eo@ya.ru>
2021-08-16 01:34:56 +0400

    sfs

--- task_1/solution.js
+++ task_1/solution.js
@@ -4,7 +4,7 @@ function addInCartMessage(title, price) {
 
     // Задание №1.1. Оповещение о добавлении в корзину
 
-    sddfвапвапше ass
+    sddfвапвапше assвапва
     // Конец решения задания №1.1.
 
     return message;
