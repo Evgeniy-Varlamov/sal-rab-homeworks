4b6cd672671e4745ad75daaef400fc4e12062854
Evgeniy-Varlamov <varlamov-eo@ya.ru>
2021-08-19 01:05:04 +0400

    asda

--- task_1/solution.js
+++ task_1/solution.js
@@ -6,7 +6,7 @@ function addInCartMessage(title, price) {
 апра
     вввfлор\\\\вапвgdfdfвfdfsdsвамв
 git config --global --unsdsdset-all user.namedssddапрпвап
-апрfghfg
+апрfghfgпвапав
 
     // Конец решения задания №1.1.
 
